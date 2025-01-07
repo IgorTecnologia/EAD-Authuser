0483e3f16165baecff6328fe4e0129d43635190d
IgorTecnologia <iguigu2019@gmail.com>
2025-01-06 21:14:56 -0300

    E-mail Constraint Impl

---
+++ src/main/java/com/ead/authuser/validations/EmailConstraintImpl.java
@@ -0,0 +1,24 @@
+package com.ead.authuser.validations;
+
+import com.ead.authuser.repositories.UserRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmailConstraintImpl implements ConstraintValidator<EmailConstraint, String> {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void initialize(EmailConstraint constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+
+        boolean existsByEmail = repository.existsByEmail(email);
+        return !existsByEmail;
+    }
+}
