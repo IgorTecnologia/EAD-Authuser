092c245ff8a2da2f93658698b523459991987d15
IgorTecnologia <iguigu2019@gmail.com>
2025-01-06 21:11:52 -0300

    User DTO

---
+++ src/main/java/com/ead/authuser/dto/UserDTO.java
@@ -0,0 +1,103 @@
+package com.ead.authuser.dto;
+
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.User;
+import com.ead.authuser.validations.CpfConstraint;
+import com.ead.authuser.validations.EmailConstraint;
+import com.ead.authuser.validations.UsernameConstraint;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO extends RepresentationModel<UserDTO> {
+
+    public interface UserView{
+        public static interface RegistrationPost {}
+        public static interface UserPut {}
+        public static interface PasswordPut {}
+        public static interface ImagePut {}
+    }
+
+    private UUID id;
+
+    @NotBlank(groups = UserView.RegistrationPost.class, message = "The username field is mandatory and blanks are not allowed.")
+    @Size(groups = UserView.RegistrationPost.class, min = 7, max = 27, message = "Minimum character value allowed is 07 and the maximum is 27.")
+    @UsernameConstraint(groups = UserView.RegistrationPost.class, message = "The username is already in use.")
+    @JsonView(UserView.RegistrationPost.class)
+    private String username;
+
+    @NotBlank(groups = UserView.RegistrationPost.class, message = "The e-mail field is mandatory and blanks are not allowed.")
+    @Email(groups = UserView.RegistrationPost.class, message = "Email must have an acceptable standard.")
+    @EmailConstraint(groups = UserView.RegistrationPost.class, message = "The e-mail is already in use.")
+    @JsonView(UserView.RegistrationPost.class)
+    private String email;
+
+    @Size(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class}, min = 7, max = 37, message = "Minimum character value allowed is 07 and the maximum is 37.")
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class}, message = "The password field is mandatory and blanks are not allowed.")
+    @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @Size(groups = UserView.PasswordPut.class, min = 7, max = 37, message = "Minimum character value allowed is 07 and the maximum is 37.")
+    @NotBlank(groups = UserView.PasswordPut.class, message = "The old password field is mandatory and blanks are not allowed.")
+    @JsonView(UserView.PasswordPut.class)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String oldPassword;
+
+    @Size(min = 7, max = 57, message = "Minimum character value allowed is 07 and the maximum is 57.")
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
+    private String fullName;
+
+    @Size(min = 11, max = 57, message = "Minimum character value allowed is 07 and the maximum is 57.")
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
+    private String phoneNumber;
+
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.UserPut.class}, message = "The CPF field is mandatory and and blanks are not allowed.")
+    @CpfConstraint(groups = {UserView.RegistrationPost.class, UserView.UserPut.class}, message = "The CPF is already in use.")
+    @Size(groups = {UserView.RegistrationPost.class, UserView.UserPut.class}, min = 7, max = 27, message = "Minimum character value allowed is 07 and the maximum is 27.")
+    @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
+    private String cpf;
+
+    @NotBlank(groups = UserView.ImagePut.class, message = "The image field is mandatory and blanks are not allowed.")
+    @JsonView(UserView.ImagePut.class)
+    private String imageUrl;
+
+    private UserStatus userStatus;
+    private UserType userType;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime creationDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime lastUpdateDate;
+
+    public UserDTO(){
+    }
+
+    public UserDTO(User entity){
+        id = entity.getId();
+        username = entity.getUsername();
+        email = entity.getEmail();
+        password = entity.getPassword();
+        fullName = entity.getFullName();
+        userStatus = entity.getUserStatus();
+        userType = entity.getUserType();
+        phoneNumber = entity.getPhoneNumber();
+        cpf = entity.getCpf();
+        imageUrl = entity.getImageUrl();
+        creationDate = entity.getCreationDate();
+        lastUpdateDate = entity.getLastUpdateDate();
+    }
+}
