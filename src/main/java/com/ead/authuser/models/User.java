ae1a3b3c27730a702160dc92202e4108e1383d4a
IgorTecnologia <iguigu2019@gmail.com>
2025-01-06 21:11:01 -0300

    User Entity

---
+++ src/main/java/com/ead/authuser/models/User.java
@@ -0,0 +1,67 @@
+package com.ead.authuser.models;
+
+import com.ead.authuser.enums.UserStatus;
+import com.ead.authuser.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 77)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 77)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    @JsonIgnore
+    private String password;
+
+    @Column(nullable = false, length = 177)
+    private String fullName;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    @Column(length = 27)
+    private String phoneNumber;
+
+    @Column(length = 27)
+    private String cpf;
+
+    @Column
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+
+    @Column(nullable = false)
+    private LocalDateTime lastUpdateDate;
+}
