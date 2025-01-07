06a5e29f3681f5c8c7daa1fdeb9d27095db098dc
IgorTecnologia <iguigu2019@gmail.com>
2025-01-06 21:18:57 -0300

    Field Message

---
+++ src/main/java/com/ead/authuser/controllers/exceptions/FieldMessage.java
@@ -0,0 +1,19 @@
+package com.ead.authuser.controllers.exceptions;
+
+import lombok.Data;
+
+@Data
+public class FieldMessage {
+
+    private String fieldName;
+    private String fieldMessage;
+
+    public FieldMessage(){
+    }
+
+    public FieldMessage(String fieldName, String fieldMessage){
+
+        this.fieldName = fieldName;
+        this.fieldMessage = fieldMessage;
+    }
+}
