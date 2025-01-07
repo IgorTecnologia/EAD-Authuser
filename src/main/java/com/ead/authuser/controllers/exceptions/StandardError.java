6d1cc1455e8565bb936db6bfe2bbebdd8a72e594
IgorTecnologia <iguigu2019@gmail.com>
2025-01-06 21:18:28 -0300

    Standard Error

---
+++ src/main/java/com/ead/authuser/controllers/exceptions/StandardError.java
@@ -0,0 +1,18 @@
+package com.ead.authuser.controllers.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class StandardError {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime timeStamp;
+
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
