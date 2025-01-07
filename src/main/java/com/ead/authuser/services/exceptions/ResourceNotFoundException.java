e98b5e68e4c544dfb585f4f61ad308cf47954281
IgorTecnologia <iguigu2019@gmail.com>
2025-01-06 21:17:51 -0300

    Resource Not Found Exception

---
+++ src/main/java/com/ead/authuser/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ead.authuser.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+}
