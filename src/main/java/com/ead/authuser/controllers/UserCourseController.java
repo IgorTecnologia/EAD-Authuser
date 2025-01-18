1efa256dd164ae47b738449e66c3aca4979a2593
IgorTecnologia <iguigu2019@gmail.com>
2025-01-17 21:22:20 -0300

    User Course Controller

---
+++ src/main/java/com/ead/authuser/controllers/UserCourseController.java
@@ -0,0 +1,35 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.clients.UserClient;
+import com.ead.authuser.dto.CourseDTO;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@Log4j2
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3700)
+public class UserCourseController {
+
+    @Autowired
+    private UserClient userClient;
+
+    @GetMapping(value = "/users/{userId}/courses")
+    public ResponseEntity<Page<CourseDTO>> findAllCoursesByUser(@PageableDefault(page = 0, size = 12, sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
+                                                                @PathVariable(value = "userId") UUID userId){
+
+        Page<CourseDTO> page = userClient.findAllCoursesByUser(userId, pageable);
+
+        return ResponseEntity.ok().body(page);
+    }
+}
