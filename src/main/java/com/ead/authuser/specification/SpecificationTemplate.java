1bf5179fb3c8254eb81c12c8691e6c004afeb0c3
IgorTecnologia <iguigu2019@gmail.com>
2025-01-17 21:22:32 -0300

    Specification Template

--- src/main/java/com/ead/authuser/specification/SpecificationTemplate.java
+++ src/main/java/com/ead/authuser/specification/SpecificationTemplate.java
@@ -1,11 +1,15 @@
 package com.ead.authuser.specification;
 
 import com.ead.authuser.models.User;
+import com.ead.authuser.models.UserCourse;
+import jakarta.persistence.criteria.Join;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.util.UUID;
 
 public class SpecificationTemplate {
 
@@ -18,5 +22,13 @@ public class SpecificationTemplate {
             @Spec(path = "phoneNumber", spec = Equal.class),
             @Spec(path = "cpf", spec = Equal.class)})
     public interface UserSpec extends Specification<User> {
+    }
+
+    public static Specification<User> userCourseId(final UUID courseId){
+        return(root, query, cb) -> {
+               query.distinct(true);
+               Join<User,UserCourse> userProd = root.join("userCourses");
+               return cb.equal(userProd.get("courseId"), courseId);
+        };
     }
 }
