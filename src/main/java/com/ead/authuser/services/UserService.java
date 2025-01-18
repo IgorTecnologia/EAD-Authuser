77c1723fd661b1afebbb8f790ca023db0dfb300a
IgorTecnologia <iguigu2019@gmail.com>
2025-01-17 21:19:11 -0300

    User Service

--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -1,15 +1,16 @@
 package com.ead.authuser.services;
 
 import com.ead.authuser.dto.UserDTO;
-import com.ead.authuser.specification.SpecificationTemplate;
+import com.ead.authuser.models.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.UUID;
 
 public interface UserService {
 
-    Page<UserDTO> findAllPaged(SpecificationTemplate.UserSpec spec, Pageable pageable);
+    Page<UserDTO> findAllPaged(Specification<User> spec, Pageable pageable);
 
     UserDTO findById(UUID id);
 
