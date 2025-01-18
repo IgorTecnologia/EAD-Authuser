c618a3f3678b89a734d7afe0366c358e0b61ca7c
IgorTecnologia <iguigu2019@gmail.com>
2025-01-17 21:15:32 -0300

    User Entity

--- src/main/java/com/ead/authuser/models/User.java
+++ src/main/java/com/ead/authuser/models/User.java
@@ -8,14 +8,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -64,6 +67,9 @@ public class User implements Serializable {
 
     @Column(nullable = false)
     private LocalDateTime lastUpdateDate;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private List<UserCourse> userCourses;
 
     public User(){
     }
